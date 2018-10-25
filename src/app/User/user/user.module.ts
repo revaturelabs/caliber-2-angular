76e50bef0b0bd2e5cc9a65a257e7c701e6394ddb
jacques myette <myette.j@husky.neu.edu>
2018-10-25 19:38:01 -0400

    deleted wrong components folder

--- src/app/User/user/user.module.ts
+++ src/app/User/user/user.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ViewtraineesComponent } from './Components/viewtrainees/viewtrainees.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [ViewtraineesComponent],
   exports: [
