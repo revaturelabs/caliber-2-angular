0c99cef4bbd027f86061c5254f1fecefa3c5cb4d
cTre19 <charlie.19.ct@gmail.com>
2018-10-31 15:27:49 -0400

    added batch component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { HomeComponent } from './home/home.component';
+import { BatchViewComponent } from './Team2/batch-view/batch-view.component';
 
 
 
 const routes: Routes = [
   { path: '', redirectTo: '#/vp/home', pathMatch: 'full' },
   { path: '#/vp/home', component: HomeComponent },
-  { path: '#/vp/manage', component: ManageComponent }
+  { path: '#/vp/manage', component: BatchViewComponent }
 ];
 
 @NgModule({
