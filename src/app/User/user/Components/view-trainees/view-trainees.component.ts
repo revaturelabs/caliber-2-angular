51cdab58aeaecb5e37160d70f4bb79fbae79df16
Ben <>
2018-11-01 13:36:30 -0400

    Finished merging our module with the rest of the project.

--- src/app/User/user/Components/view-trainees/view-trainees.component.ts
+++ src/app/User/user/Components/view-trainees/view-trainees.component.ts
@@ -27,8 +27,7 @@ export class ViewTraineesComponent implements OnInit {
   none = TraineeFlag.NONE;
 
   constructor(
-    private ts: TraineesService,
-    private http: HttpClient) { }
+    private ts: TraineesService) { }
 
   /**
    * Uses lifecycle hook ngOnInit to intialize mock trainees for testing
