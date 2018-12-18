14d039530f513031ff64644d62310deffe2a252d
groman-gonzalez <gerald.romangonzalez@gmail.com>
2018-12-18 16:48:57 -0500

    Audit directory added with the associate, tools, and overall components

---
+++ src/app/Audit/Components/overall/overall.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overall',
+  templateUrl: './overall.component.html',
+  styleUrls: ['./overall.component.css']
+})
+export class OverallComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
