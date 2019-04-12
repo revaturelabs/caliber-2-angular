44de41a2f674a89b4716858bccf33cc0932ab99f
dattran91 <dattran91@hotmail.com>
2019-04-12 14:08:10 -0400

    fix untrack component

---
+++ src/app/Assess-Batch/Components/toolbar/toolbar.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AuditService } from 'src/app/Audit/Services/audit.service';
+import { Batch } from 'src/app/Batch/type/batch';
+import { BatchModalComponent } from '../../batch-modal/batch-modal.component';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent implements OnInit {
+
+  years: number[];
+  batches: Batch[];
+  selectedBatches: Batch[];
+  defaultYears: number[];
+  selectedYear: number;
+  selectedBatch: Batch;
+  selectedBatchId = 0;
+  weeks = [];
+  selectedWeek: number;
+  createUpdate: Batch = null;
+  @ViewChild('batchModal') batchModal: BatchModalComponent;
+
+
+  constructor(
+    public auditService: AuditService
+  ) { }
+
+  ngOnInit() {
+    
+    this.selectedWeek=1;
+    this.getAllYears();
+
+  }
+
+     /**
+   * resets createorUpdate variable for child component
+   */
+  resetBatchForm(): void {
+    this.createUpdate = null;
+    this.batchModal.resetForm();
+  }
+
+  // ToDo: future implementation
+  // method for import button
+  resetImportModal(): void {
+
+  }
+
+
+  getAllYears() {
+    this.auditService.getAllYears()
+    .subscribe(result => {
+      this.years = result;
+      this.selectedYear = this.years[0];
+      console.log(this.years);
+      this.getBatches();
+    });
+    
+  }
+
+  getBatches() {
+    this.auditService.getBatchesByYear(this.selectedYear)
+    .subscribe(result => {
+      this.batches = result;
+      this.selectedBatch = this.batches[0];
+      this.auditService.selectedBatch = this.batches[0];
+      console.log(this.batches);
+      this.getWeeks();
+      });
+      
+  }
+
+  selectYear(event: number) {
+    this.selectedYear = event;
+    this.auditService.selectedYear = this.selectedYear;
+    this.auditService.getBatchesByYear(event)
+    .subscribe(result => {
+      this.batches = result;
+      });
+  }
+
+  selectBatch(event: Batch) {
+    this.selectedBatch = event;
+    this.auditService.selectedBatch = this.selectedBatch;
+    this.getWeeks();
+  }
+
+  showActiveWeek(week: number) {
+    if (week==this.selectedWeek) {
+      return "active";
+    }
+  }
+
+  selectWeek(event: number) {
+    this.selectedWeek = event;
+    this.auditService.selectedWeek = event;
+  }
+
+  addWeek() {
+    var last = this.weeks[this.weeks.length-1];
+    this.weeks.push(last+1);
+    this.selectedWeek=last+1;
+  }
+
+  getWeeks() {
+    this.weeks = [];
+    for(var i = 0; i<this.selectedBatch.weeks; i++){
+      this.weeks.push(i+1);
+    }
+  }
+
+}
---
+++ src/app/Assess-Batch/batch-modal/batch-modal.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+
+import { BatchModalComponent } from './batch-modal.component';
+import { BatchService } from '../batch.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { Batch } from '../type/batch';
+import { ErrorService } from 'src/app/error-handling/services/error.service';
+
+describe('BatchModalComponent', () => {
+  let component: BatchModalComponent;
+  let testService: BatchService;
+  let errorService: ErrorService;
+
+  beforeEach(() => {
+    component = new BatchModalComponent(testService, errorService);
+  });
+
+  it('should create a component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have goodGradeThreshold be undefined', () => {
+    expect(component.goodGradeThreshold).toBeUndefined();
+  });
+
+  it('should have goodGradeThreshold not be undefined', () => {
+    component.goodGradeThreshold = 1;
+    expect(component.goodGradeThreshold).toBeDefined();
+  });
+
+  it('should have goodGradeThreshold be equal to borderlineGradeThreshold', () => {
+    component.goodGradeThreshold = 1;
+    component.setMinGrade();
+    expect(component.borderlineGradeThreshold).toBeGreaterThanOrEqual(component.goodGradeThreshold);
+  });
+
+  it('should have borderlineGradeThreshold not be greater than goodGradeThreshold', () => {
+    component.goodGradeThreshold = 3;
+    component.borderlineGradeThreshold = 4;
+    component.lowerMinGrade();
+    expect(component.borderlineGradeThreshold).toBeLessThanOrEqual(component.goodGradeThreshold);
+  });
+});
+
+
