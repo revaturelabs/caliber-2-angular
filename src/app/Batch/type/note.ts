8a593c7fdacd2fbd56257df42e8b15c02d3f8c7e
vijaysadanani <vksadanani@gmail.com>
2019-04-17 10:47:06 -0400

    CAL-160 Added note.ts as a class

---
+++ src/app/Batch/type/note.ts
@@ -0,0 +1,10 @@
+export class Note {
+    noteId: number;
+    noteContent: string;
+    noteType: string;
+    weekNumber: number;
+    batchId: number;
+    traineeId: number;
+    
+    constructor() {}
+}
