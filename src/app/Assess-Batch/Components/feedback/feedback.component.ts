4ca42112bafe3347fab31280625112404be682dd
djbogar <46689626+djbogar@users.noreply.github.com>
2019-04-17 16:10:37 -0400

    added funtionality to get batch feedback note for the week and the textarea for the note in the html

--- src/app/Assess-Batch/Components/feedback/feedback.component.ts
+++ src/app/Assess-Batch/Components/feedback/feedback.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Note } from 'src/app/Batch/type/note';
+import { BatchModalComponent } from '../../batch-modal/batch-modal.component';
+import { NoteService } from '../../Services/note.service';
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.css']
+ selector: 'app-feedback',
+ templateUrl: './feedback.component.html',
+ styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent implements OnInit {
+ notes: Note[];
+ selectedWeek: number;
+ feedbackNote: Note;
+ selectedBatchId: number;
+ selectedWeekId: NumberConstructor;
+ traineeId: number;
+ @ViewChild('batchModal') batchModal: BatchModalComponent;
 
-  constructor() { }
+ constructor(private noteService: NoteService) { }
 
-  ngOnInit() {
-  }
+//provides the traineer feedback note for the week
+getFeedbackNote(selectedBatchId, selectedWeek){
+  //get all batch notes for the week
+  this.noteService.getBatchNotesByWeek(selectedBatchId, selectedWeek)
+  .subscribe(result => {
+    //narrow down to the singlar feedback note
+    for(let n of result){
+      if(n.noteType == 'BATCH'){
+        this.notes.push(n);
+      }
+    }
+    if(this.notes.length = 1){
+      this.feedbackNote = this.notes[0];
+    }
+    console.log(this.notes);
+  });
+ }
 
+ //updates feedback note on blur
+updateFeedbackNote(){
+  this.noteService.putNote(this.feedbackNote);
+ }
+
+ngOnInit() {
+
+ }
 }
+
--- src/app/Assess-Batch/Services/note.service.ts
+++ src/app/Assess-Batch/Services/note.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Note } from 'src/app/Batch/type/note';
+import { HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +20,20 @@ export class NoteService {
    yearsURL = '/all/batch/valid_years';
    noteAllURL = '/all/notes/all';
   noteURL = '/all/notes/';
-  
- 
+  batchNotesByWeekUrl='/all/notes/all?batch=';
+  noteEmitter = new EventEmitter<Note[]>();
+
    getAllNotes(): Observable<Note[]>{
     return this.http.get<Note[]>(this.url + this.noteAllURL);
+  }
+
+  getBatchNotesByWeek(batchId: number, weekId: number): Observable<Note[]>{
+    console.log(batchId);
+    console.log(weekId);
+    return this.http.get<Note[]>(this.url + this.batchNotesByWeekUrl +batchId + "&week=" + weekId);
+  }
+
+  putNote(note: Note): Observable<Note>{
+    return this.http.put<Note>(this.url, note, httpOptions);
   }
 }
