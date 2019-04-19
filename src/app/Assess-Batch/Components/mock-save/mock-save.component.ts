383d110c3841498787806a1d7e30cd018036d663
djbogar <46689626+djbogar@users.noreply.github.com>
2019-04-19 11:04:20 -0400

    CAL156 added timeout functions and displaying all three icon correctly will need to add a fade to checkmark

--- src/app/Assess-Batch/Components/mock-save/mock-save.component.ts
+++ src/app/Assess-Batch/Components/mock-save/mock-save.component.ts
@@ -9,8 +9,15 @@ export class MockSaveComponent implements OnInit {
 
   constructor() { }
 
+  showSaving = false;
+  showCheck = false;
+  showFloppy = true;
+
   doBurrito(){
-    
+    this.showFloppy = false;
+    this.showSaving = true;
+    setTimeout(()=> {this.showSaving = false; this.showCheck = true}, 1000);
+    setTimeout(()=> {this.showCheck = false; this.showFloppy = true}, 2000);
   }
 
   ngOnInit() {
