ff1a35bae4509656d583053099dba7e4bf684b52
Hassan Raqib <hraqib1@gmail.com>
2019-04-22 12:12:04 -0400

    fixed populating grades

--- src/app/Assess-Batch/Services/assess-batch-grades.service.ts
+++ src/app/Assess-Batch/Services/assess-batch-grades.service.ts
@@ -8,34 +8,38 @@ import { traineeAssessment, Grade } from '../../User/user/types/trainee'
   providedIn: 'root'
 })
 export class AssessBatchGradeService {
-
   url = 'http://localhost:9097';
   gradesByIdURL = '/all/grade/batch/';
+  gradeAll = '/all/grade/all';
   assessmentsByIdURL = '/all/assessment/batch/';
+
   allAssessments: traineeAssessment[] = [];
   allGrades: Grade[] = [];
   assessments = new EventEmitter<traineeAssessment[]>();
   grades = new EventEmitter<Grade[]>();
 
-  constructor(private http: HttpClient) { }
 
+  constructor(private http: HttpClient) { }
   getAssessmentsByBatchId(id: number): Observable<traineeAssessment[]> {
     return this.http.get<traineeAssessment[]>(this.url + this.assessmentsByIdURL + id);
   }
-
-  getGradesByBatchId(id: number): Observable<number[]> {
-    return this.http.get<number[]>(this.url + this.gradesByIdURL);
+  getGradesByBatchId(id: number): Observable<Grade[]> {
+    return this.http.get<Grade[]>(this.url + this.gradesByIdURL + id);
   }
-
   storeAssessments(entry: traineeAssessment[]) {
     this.allAssessments = entry;
     console.log(this.allAssessments);
   }
-
   returnAssessments(): traineeAssessment[] {
     return this.allAssessments;
   }
-
-
+  storeGrades(entry: Grade[]) {
+    this.allGrades = entry;
+    console.log(this.allGrades);
+  }
+  returnGrades(): Grade[] {
+    return this.allGrades;
+  }
   
 }
+
