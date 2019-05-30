1d3b117c9f3dc8e51be5c33bc98263f40740d8b0
Sal Calabrese <erotavlas103@gmail.com>
2019-05-30 13:16:15 -0400

    working on functionality

--- src/app/Audit/Components/overall/overall.component.ts
+++ src/app/Audit/Components/overall/overall.component.ts
@@ -36,21 +36,21 @@ export class OverallComponent implements OnInit, OnDestroy {
 	}
 
 	setScore(qcStatus: string, noteId: number) {
-		
-		// this.auditService.sendNote(this.note).subscribe(
-		// 	data => {
-		// 		this.auditService.getOverallBatchNoteByWeek(this.auditService.selectedBatch['batchId'], this.auditService.selectedWeek);
-		// 	},
-		// 	issue => {
-		// 		if (issue instanceof HttpErrorResponse) {
-		// 			const err = issue as HttpErrorResponse;
-		// 			this.errorService.setError('AuditService',
-		// 				`Issue updating QcNote with noteId ${this.note.noteId}. Please contact system administrator: \n
-		// 		  Status Code: ${err.status} \n
-		// 		  Status Text: ${err.statusText} \n
-		// 		  Error: ${err.message}`);
-		// 		}
-		// 	});
+		console.log(qcStatus)
+		this.auditService.sendNote(this.note).subscribe(
+			data => {
+				this.auditService.getOverallBatchNoteByWeek(this.auditService.selectedBatch['batchId'], this.auditService.selectedWeek);
+			},
+			issue => {
+				if (issue instanceof HttpErrorResponse) {
+					const err = issue as HttpErrorResponse;
+					this.errorService.setError('AuditService',
+						`Issue updating QcNote with noteId ${this.note.noteId}. Please contact system administrator: \n
+				  Status Code: ${err.status} \n
+				  Status Text: ${err.statusText} \n
+				  Error: ${err.message}`);
+				}
+			});
 
 	}
 
