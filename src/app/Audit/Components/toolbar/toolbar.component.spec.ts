9bd7ee2f55f2d71755a6fd21d6a332c4a7f58be8
Michael Araujo <michael.e.araujo@gmail.com>
2019-05-30 09:51:17 -0400

    fixed some old tests

--- src/app/Audit/Components/toolbar/toolbar.component.spec.ts
+++ src/app/Audit/Components/toolbar/toolbar.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ToolbarComponent } from './toolbar.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ToolbarComponent', () => {
   let component: ToolbarComponent;
@@ -8,7 +9,8 @@ describe('ToolbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ToolbarComponent ]
+      declarations: [ ToolbarComponent ],
+      imports: [HttpClientTestingModule],
     })
     .compileComponents();
   }));
--- src/app/Audit/Services/audit.service.spec.ts
+++ src/app/Audit/Services/audit.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuditService } from './audit.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuditService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [AuditService],
+  }));
 
   it('should be created', () => {
     const service: AuditService = TestBed.get(AuditService);
