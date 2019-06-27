a85a9b44e91287e6d21fe7a2adf70b78c48e834d
Kenneth Tsai <kennethtsai7@gmail.com>
2019-06-26 20:10:56 -0400

    added bar-chart and assessment-breakdown component

---
+++ src/app/reports/Components/assessment-breakdown/assessment-breakdown.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-assessment-breakdown',
+  templateUrl: './assessment-breakdown.component.html',
+  styleUrls: ['./assessment-breakdown.component.css']
+})
+export class AssessmentBreakdownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
