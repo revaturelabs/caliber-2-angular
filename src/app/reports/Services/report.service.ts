d6eac0ec6779e315083913154071824e1850ca7b
James Kellenberger <jwkellenberger@gmail.com>
2019-06-27 20:46:27 -0400

    restructures how the query of reports page works for batch pulls

--- src/app/reports/Services/report.service.ts
+++ src/app/reports/Services/report.service.ts
@@ -3,6 +3,8 @@ import { environment } from 'src/environments/environment.prod';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Batch } from 'src/app/Batch/type/batch';
+import { traineeAssessment, Grade } from 'src/app/Batch/type/trainee';
+import { Assessment } from 'src/app/Assess-Batch/Models/Assesment';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,9 +18,11 @@ const httpOptions = {
 export class ReportService {
   // url = environment.serverRootURL;
   url = 'http://localhost:10000';
-  batchesYearURL = '/qc/batch/';
+  batchesYearURL = '/vp/batch/';
   batchAllURL = 'batchAllURL';
   yearsURL = '/qa/batch/valid-years';
+  gradesAllURL = '/assessment/all/grade/all';
+  assessmentsAllURL : string = '/assessment/all/assessment/all';
   selectedYear: number;
   constructor(private http: HttpClient) { }
 
@@ -28,5 +32,13 @@ export class ReportService {
 
   getBatchesByYear(year : number): Observable<Batch[]> {
     return this.http.get<Batch[]>(this.url +'/batch' + this.batchesYearURL + year, httpOptions);
+  }
+
+  getAllAssessments():Observable<Assessment[]> {
+    return this.http.get<Assessment[]>(this.url + this.assessmentsAllURL, httpOptions)
+  }
+
+  getAllGrades():Observable<Grade[]> {
+    return this.http.get<Grade[]>(this.url + this.gradesAllURL, httpOptions)
   }
 }
