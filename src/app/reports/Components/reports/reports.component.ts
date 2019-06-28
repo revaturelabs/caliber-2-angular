dc5f4cc6f33c88393e41426385df32593e3d981c
James Kellenberger <jwkellenberger@gmail.com>
2019-06-28 00:45:35 -0400

    added eventemitter support for report

--- src/app/reports/Components/reports/reports.component.ts
+++ src/app/reports/Components/reports/reports.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReportOutput } from '../../Models/report-output';
 
 @Component({
   selector: 'app-reports',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
-
+  reportOutput : ReportOutput;
   constructor() { } 
 
   ngOnInit() {
   }
 
+  updateReportOutput(reportOutput: ReportOutput){
+    this.reportOutput = reportOutput;
+    console.log("I got it! " + this.reportOutput.calculateAssessmentsAverage);
+  }
 }
