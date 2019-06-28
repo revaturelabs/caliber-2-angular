3f7b00a4ba716a84d293c289edafa47586761d43
Taylor Adams <tdadams0@gmail.com>
2019-06-27 21:16:03 -0400

    Paired data labels in bar chart component

--- src/app/reports/Components/bar-chart/bar-chart.component.ts
+++ src/app/reports/Components/bar-chart/bar-chart.component.ts
@@ -40,9 +40,13 @@ export class BarChartComponent implements OnInit {
             };
             */
   private defaultBackgroundColor = 'rgba(114, 164, 194, .5)';
+  private defaultBorderColor = 'rgba(114, 164, 194, 1)';
+
+  private defaultBackgroundColor2 = 'rgba(252, 180, 20, .6)';
+  private defaultBorderColor2 = 'rgba(252, 180, 20, 1)';
+
   private defaultBorderWidth = 3;
-  private defaultBorderColor = "#ffcc00";
-  private defaultMinimumYAxis = 40;
+  private defaultMinimumYAxis = 0; //40
   private defaultMaxYAxis = 100;
   private defaultStepSize = 20;
 
@@ -54,18 +58,20 @@ export class BarChartComponent implements OnInit {
   
   private chartDatasets: Array<any> = [
     { 
-      data: [ 12,2,1 ],
+      data: [ 12,8,9],
       fill: false,
       backgroundColor: this.chartColors,
       borderColor: this.defaultBorderColor,
-      borderWidth: this.defaultBorderWidth
+      borderWidth: this.defaultBorderWidth,
+      label: ''
     },
     { 
-      data: [ 14,5,1],
+      data: [12,8,9],
       fill: false,
       backgroundColor: this.chartColors,
       borderColor: this.defaultBorderColor,
-      borderWidth: this.defaultBorderWidth
+      borderWidth: this.defaultBorderWidth,
+      label: ''
     },
   ];
   
@@ -83,7 +89,7 @@ export class BarChartComponent implements OnInit {
     this.createChart() 
   }
   
-  public addDataPoint(data : number, pairWithDataset = 0, backColor = this.defaultBackgroundColor, 
+  public addDataPoint(data : number, pairWithDataset = 0, label : string, backColor = this.defaultBackgroundColor, 
     borderColor = this.defaultBorderColor, borderWidth = this.defaultBorderWidth) : void
   {
     
@@ -97,6 +103,7 @@ export class BarChartComponent implements OnInit {
         this.chartDatasets[i]["backgroundColor"].push(backColor);
         this.chartDatasets[i]["borderColor"] = borderColor;
         this.chartDatasets[i]["borderWidth"] = borderWidth;
+        this.chartDatasets[i]["label"] = label;
       }
     }
 
@@ -104,11 +111,12 @@ export class BarChartComponent implements OnInit {
     { //This occurs when there was no empty spot, and so the maximum category points has to be increased.
       this.incrementMaximumCategoryPoints();
       let index = this.chartDatasets.length - 1;
-
+      
       this.chartDatasets[index]["data"][pairWithDataset] = data;
       this.chartDatasets[index]["backgroundColor"].push(backColor);
       this.chartDatasets[index]["borderColor"] = borderColor;
       this.chartDatasets[index]["borderWidth"] = borderWidth;
+      this.chartDatasets[index]["label"] = label;
     }
   }
 
@@ -134,7 +142,8 @@ export class BarChartComponent implements OnInit {
       borderColor: this.defaultBorderColor,
       borderWidth: this.defaultBorderWidth,
       fill: false,
-      backgroundColor: [ ]
+      backgroundColor: [ ],
+      label: ''
     })
   }
 
@@ -146,7 +155,7 @@ export class BarChartComponent implements OnInit {
       }
     ];
     this.chartLabels = [ ];
-    //this.chartDatasets = [ ];
+    this.chartDatasets = [ ];
     
     /*
     this.chart = new Chart('canvas', {
@@ -180,6 +189,16 @@ this.chart = new Chart('canvas', {
     datasets: this.chartDatasets
   },
   options: {
+    title: {
+      display: true,
+      text: "Sample title"
+    },
+    tooltips: {
+      mode: 'label',
+      callbacks:{
+        //Callback function could go here, to say, dynamically generate a label.
+      }
+    },
     legend: {
       display: true
     },
@@ -205,20 +224,33 @@ this.chart = new Chart('canvas', {
 
     
     this.initializeChart('bar');
-    this.addDataset("Category 1");
-    this.addDataset("Category 2");
-    this.addDataset("Category 3");
-    this.addDataPoint(12, 0);
-    this.addDataPoint(11, 1);
-    this.addDataPoint(10, 0);
-    this.addDataPoint(9, 1);
-    this.addDataPoint(8, 0);
-    this.addDataPoint(7, 1);
-    this.addDataPoint(6, 0);
-    this.addDataPoint(5, 1);
-    this.addDataPoint(4, 0);
+    this.addDataset("Projects");
+    this.addDataset("Exam");
+    this.addDataset("Other");
+    this.addDataset("Verbal");
+    this.addDataPoint(78, 0, 'Trainee');
+    this.addDataPoint(92, 0, 'Batch', this.defaultBackgroundColor2, this.defaultBorderColor2);
+    
+   this.addDataPoint(75, 1, 'Trainee');
+    this.addDataPoint(82, 1, 'Batch', this.defaultBackgroundColor2, this.defaultBorderColor2);
+    
+    this.addDataPoint(49, 2, 'Trainee');
+   this.addDataPoint(59, 2, 'Batch', this.defaultBackgroundColor2, this.defaultBorderColor2);
+    
+    this.addDataPoint(88.5, 3, 'Trainee');
+    this.addDataPoint(89.5, 3, 'Batch', this.defaultBackgroundColor2, this.defaultBorderColor2);
+
+    // this.addDataPoint(11, 1 , 'Test1');
+    // this.addDataPoint(10, 0, 'Test1');
+    // this.addDataPoint(9, 1, 'Test1');
+    // this.addDataPoint(8, 0, 'Test1');
+    // this.addDataPoint(7, 1, 'Test1');
+    // this.addDataPoint(6, 0, 'Test1');
+    // this.addDataPoint(5, 1, 'Test1');
+    // this.addDataPoint(4, 0, 'Test1');
     console.log(this.chartDatasets);
     console.log(this.chart);
 
   }
+
 }
