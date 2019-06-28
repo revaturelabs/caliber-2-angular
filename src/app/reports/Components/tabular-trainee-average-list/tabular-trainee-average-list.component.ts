22b08dbb3b71726bda0fedef760a78fe48eb283a
James Kellenberger <jwkellenberger@gmail.com>
2019-06-28 01:08:15 -0400

    changed tabular trainee average to properly print assessment score

--- src/app/reports/Components/tabular-trainee-average-list/tabular-trainee-average-list.component.ts
+++ src/app/reports/Components/tabular-trainee-average-list/tabular-trainee-average-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ReportOutput } from '../../Models/report-output';
+import { Grade } from 'src/app/User/user/types/trainee';
 
 @Component({
   selector: 'app-tabular-trainee-average-list',
@@ -7,9 +8,16 @@ import { ReportOutput } from '../../Models/report-output';
   styleUrls: ['./tabular-trainee-average-list.component.css']
 })
 export class TabularTraineeAverageListComponent implements OnInit {
-@Input() reportOutput: ReportOutput;
+@Input() reportOutput: ReportOutput = new ReportOutput();
+
   constructor() { }
 
   ngOnInit() {
+    let newGrade = new Grade();
+    newGrade.score=0;
+    newGrade.traineeId=-1;
+    newGrade.assessmentId=-1;
+
+    this.reportOutput.gradesDataStore = [newGrade];
   }
 }
