c8d6a82e45b67c255aa85c6b89648e5931e02158
James Kellenberger <jwkellenberger@gmail.com>
2019-07-01 00:01:52 -0400

    added data queries to service in preparation for toolbar

--- src/app/reports/Service/report.service.ts
+++ src/app/reports/Service/report.service.ts
@@ -5,7 +5,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Batch } from 'src/app/Batch/type/batch';
 import { traineeAssessment, Grade } from 'src/app/Batch/type/trainee';
 import { Assessment } from 'src/app/Assess-Batch/Models/Assesment';
-import { Trainee } from 'src/app/User/user/types/trainee';
+import { Trainee, Category } from 'src/app/User/user/types/trainee';
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -23,18 +23,27 @@ export class ReportService {
   batchAllURL = 'batchAllURL';
   yearsURL = '/qa/batch/valid-years';
   gradesAllURL = '/assessment/all/grade/batch/';
+  qaNotesAllURL = 'qa/audit/notes/all/';
+  qaNotesURL = 'qa/audit/notes/';
+  categoryAllURL = '/category/all/active';
   assessmentsAllURL : string = '/assessment/all/assessment/batch/';
 
   batch: Batch;
   week: number;
   trainee:Trainee;
+  batchDataStore : Batch[];
   gradesDataStore : Grade[];
+  traineeDataStore: Trainee[];
   assessmentsDataStore : Assessment[];
   
   constructor(private http: HttpClient) { }
 
   getAllYears(): Observable<number[]> {
     return this.http.get<number[]>(this.url + this.yearsURL);
+  }
+
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(this.url + this.categoryAllURL);
   }
 
   getBatchesByYear(year : number): Observable<Batch[]> {
@@ -51,8 +60,29 @@ export class ReportService {
     return this.http.get<Grade[]>(this.url + this.gradesAllURL + this.batch.batchId + weekStr, httpOptions)
   }
 
+  // getAllQANotes():Observable<QANote[]> {
+  //   if(this.week == 0)
+  //     return this.http.get<QANote[]>(this.url + this.qaNotesAllURL + this.batch.batchId, httpOptions)
+  //   else{
+  //     return this.http.get<QANote[]>(this.url + this.qaNotesURL + this.batch.batchId +"/"+this.week, httpOptions)
+  //   }
+  // }
+
+
+
+  //// add a query for all weeks
+  //// http://localhost:10000/qa/audit/notes/2050/1
+
+  setBatchDataStore(batchDataStore: Batch[]){
+    this.batchDataStore = batchDataStore;
+  }
+
   setGradeDataStore(gradesDataStore: Grade[]){
     this.gradesDataStore = gradesDataStore;
+  }
+
+  setTraineeDataStore(traineeDataStore: Trainee[]){
+    this.traineeDataStore = traineeDataStore;
   }
 
   setAssessmentDataStore(assessmentDataStore: Assessment[]){
@@ -70,19 +100,35 @@ export class ReportService {
     this.batch = batch;
   }
 
+  getBatch(){
+    return this.batch
+  }
+
   setWeek(week){
     this.week = week;
+  }
+
+  getWeek():number{
+    return this.week
   }
 
   setTrainee(trainee){
     this.trainee = trainee;
   }
 
-  getAssessmentDataStore(): Assessment[]{
-    return this.assessmentsDataStore;
+  getBatchDataStore(): Batch[]{
+    return this.batchDataStore;
   }
 
   getGradeDataStore() : Grade[]{
     return this.gradesDataStore;
+  }
+
+  getTraineeDataStore() : Trainee[]{
+    return this.traineeDataStore;
+  }
+
+  getAssessmentDataStore(): Assessment[]{
+    return this.assessmentsDataStore;
   } 
 }
