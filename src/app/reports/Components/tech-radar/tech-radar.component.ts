09df3aa997b23d0a02c38bff36e344560c209777
Mitch6706 <MitchAndrewMiller@gmail.com>
2019-07-02 07:30:55 -0400

    committing before pulling toolbar update

--- src/app/reports/Components/tech-radar/tech-radar.component.ts
+++ src/app/reports/Components/tech-radar/tech-radar.component.ts
@@ -1,13 +1,27 @@
+import { Category } from './../../../Assess-Batch/Models/Category';
+import { Assessment } from './../../../Assess-Batch/Models/Assesment';
+import { Trainee } from './../../../Batch/type/trainee';
+import { Grade } from 'src/app/Batch/type/trainee';
+import { ReportOutput } from './../../Models/report-output';
 import { Component, OnInit } from '@angular/core';
 import { Chart, RadialChartOptions, ChartDataSets, ChartType } from 'chart.js';
 import { Label } from 'ng2-charts';
+import { ReportService } from '../../Service/report.service';
 
+
+// This radar chart is made with ng2-charts, which provides directives to use chart.js more easily.
 @Component({
   selector: 'app-tech-radar',
   templateUrl: './tech-radar.component.html',
   styleUrls: ['./tech-radar.component.css']
 })
 export class TechRadarComponent implements OnInit {
+  gradeDataStore: Grade[];
+  traineeDataStore: Trainee[];
+  assessmentDataStore: Assessment[];
+  categoryDataStore: Category[];
+  gradeAverages: number[] = [];
+
   // Radar
   public radarChartOptions: RadialChartOptions = {
     responsive: true,
@@ -20,9 +34,11 @@ export class TechRadarComponent implements OnInit {
   ];
   public radarChartType: ChartType = 'radar';
 
-  constructor() { }
+  constructor(private reportService: ReportService) {
+   }
 
   ngOnInit() {
+    this.updateDataPull();
   }
 
   // events
@@ -32,5 +48,13 @@ export class TechRadarComponent implements OnInit {
 
   public chartHovered({ event, active }: { event: MouseEvent, active: {}[] }): void {
     console.log(event, active);
+  }
+
+  public updateDataPull(){
+    this.gradeDataStore = this.reportService.getGradeDataStore();
+    this.categoryDataStore = this.reportService.getCategoryDataStore();
+    this.traineeDataStore = this.reportService.getTraineeDataStore();
+    this.assessmentDataStore = this.reportService.getAssessmentDataStore();
+
   }
 }
