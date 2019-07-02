36e22c7cff30937bca860eb3b77608210ddf63c0
Taylor Adams <tdadams0@gmail.com>
2019-07-02 18:23:19 -0400

    Changed the chart used in the assessment breakdown component to ngChart as per other components in the system.

--- src/app/reports/Components/assessment-breakdown/assessment-breakdown.component.ts
+++ src/app/reports/Components/assessment-breakdown/assessment-breakdown.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { BarLineChartComponent } from '../bar-line-chart/bar-line-chart.component';
 import { ReportService } from '../../Service/report.service';
+import { Assessment } from 'src/app/Assess-Batch/Models/Assesment';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { Trainee, Grade } from 'src/app/Batch/type/trainee';
 
 @Component({
   selector: 'app-assessment-breakdown',
@@ -21,23 +25,104 @@ export class AssessmentBreakdownComponent implements OnInit {
   public batchOther = "58.92";
   public batchVerbal = "89.45";
 
-  @ViewChild(BarLineChartComponent) chart : BarLineChartComponent;
+  public getBatchAverage = false;
 
 
-  constructor(private reportsService : ReportService, private cd: ChangeDetectorRef) { }
+  gradeDataStore :Grade[];
+  traineeDataStore : Trainee[];
+  assessmentDataStore : Assessment[];
+  gradeAverages: number[] = [];
+
+  constructor(private reportService : ReportService, private cd: ChangeDetectorRef) { }
 
   ngOnInit() {
-  }
-
-  ngAfterViewInit(){
-
-    this.chart.initializeChart();
-    this.chart.addDataset("Category 1");
-    this.chart.addDataPoint(75, "Test Data", 0);
-    this.cd.detectChanges();
   }
 
   public onDownloadClick(e: any) : void {
 
   }
+
+  public barChartOptions: ChartOptions = {
+    responsive: true,
+    scales : {
+      yAxes: [{
+        scaleLabel: {
+        display: true,
+        labelString: 'Average'
+      },
+        ticks: {
+          max : 100,
+          min : 40,
+          stepSize: 20,
+          padding: 0,
+          backdropPaddingX: 0,
+          display: true,
+          
+        }
+      }]
+    },
+  };
+  public barChartLabels: Label[] = [];
+  public barChartType: ChartType = 'bar';
+  public barChartLegend = true;
+  public barChartPlugins = [];
+  public barChartColors: Array<any> = [
+    { 
+      backgroundColor: 'rgba(114, 164, 194, .5)',
+      borderColor: 'rgba(114, 164, 194, 1)',
+      pointBackgroundColor: 'rgba(252, 180, 20, .6)',
+      pointBorderColor: 'rgba(252, 180, 20, 1)',
+      pointHoverBackgroundColor: 'rgba(252, 180, 20, .6)',
+      pointHoverBorderColor: 'rgba(252, 180, 20, 1)',
+    }]
+
+  public barChartData: ChartDataSets[] = [
+    { data: [], label: 'Series A'}
+  ];
+
+  updateDataPull(){
+    console.log("Updating Assessment Breakdown Score:");
+    this.gradeDataStore = this.reportService.getGradeDataStore();
+    this.traineeDataStore = this.reportService.getTraineeDataStore();
+    this.assessmentDataStore = this.reportService.getAssessmentDataStore();
+
+    let gradeArray=[];
+    let students=[];
+    let borderWidth=[];
+    this.traineeDataStore.forEach((element)=>{
+      gradeArray.push(0);
+      students.push(element.name);
+      borderWidth.push(2);
+    });
+
+    this.gradeDataStore.forEach((element)=>{
+      
+      let index = this.traineeDataStore.findIndex((trainee)=>{
+        return trainee.traineeId == element.traineeId
+      });
+      gradeArray[index] += element.score
+      // this.barChartLabels =[]
+      
+    });
+
+    let i:number =0;
+    for(i=0; i<gradeArray.length;i++){
+      gradeArray[i] = gradeArray[i]/this.assessmentDataStore.length;
+      gradeArray[i] = Math.round(gradeArray[i] * 100) / 100
+
+    }
+
+    console.log(gradeArray);
+    ////////////// place grade.toFixed(2) loop here
+    /////////// (maybe the issue was that it has to happen after sorting?)
+    this.barChartLabels = students;
+    this.gradeAverages = gradeArray;
+    
+    this.barChartData= [
+      { data: gradeArray, label: 'Trainee', borderWidth: borderWidth},
+      { data: gradeArray, label: 'Batch', borderWidth: borderWidth}
+    ];
+  }
+
+
 }
