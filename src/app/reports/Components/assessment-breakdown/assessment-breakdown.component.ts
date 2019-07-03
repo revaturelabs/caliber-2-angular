586f810d89e065cf071badb8d9c846aaa0b529b6
Kenneth Tsai <kennethtsai7@gmail.com>
2019-07-03 12:28:53 -0400

    implemented batch average assessments to the assessment breakdown component

--- src/app/reports/Components/assessment-breakdown/assessment-breakdown.component.ts
+++ src/app/reports/Components/assessment-breakdown/assessment-breakdown.component.ts
@@ -1,3 +1,4 @@
+import { AssessmentScore } from './../../Models/assessment-score';
 import { Component, OnInit, ViewChild, ChangeDetectorRef } from '@angular/core';
 import { BarLineChartComponent } from '../bar-line-chart/bar-line-chart.component';
 import { ReportService } from '../../Service/report.service';
@@ -12,21 +13,28 @@ import { Trainee, Grade } from 'src/app/Batch/type/trainee';
   styleUrls: ['./assessment-breakdown.component.css']
 })
 export class AssessmentBreakdownComponent implements OnInit {
-  public tableCol1 = "Project";
-  public tableCol2 = "Exam"
-  public tableCol3 = "Verbal"
-  public tableCol4 = "Other"
-  public traineeProject = "91.00";
-  public traineeExam = "84.89";
-  public traineeOther = "73.00";
-  public traineeVerbal = "91.43";
-  public batchProject = "91.92";
-  public batchExam = "81.81";
-  public batchOther = "58.92";
-  public batchVerbal = "89.45";
+  public tableHeader = [
+    "Exam",
+    "Project",
+    "Verbal",
+    "Other"
+  ];
+  public traineeRow = [
+    "91.00",
+    "84.89",
+    "73.00",
+    "91.43"
+  ];
+  public batchRow = [
+    "91.92",
+    "81.81",
+    "58.92",
+    "89.45"
+  ];
 
   public getBatchAverage = false;
 
+  public traineeSelected : boolean = false;
 
   gradeDataStore :Grade[];
   traineeDataStore : Trainee[];
@@ -81,6 +89,8 @@ export class AssessmentBreakdownComponent implements OnInit {
   ];
 
   updateDataPull(){
+    this.traineeSelected = this.reportService.trainee.batchId > 0;
+    
     console.log("Updating Assessment Breakdown Score:");
     this.gradeDataStore = this.reportService.getGradeDataStore();
     this.traineeDataStore = this.reportService.getTraineeDataStore();
@@ -88,14 +98,30 @@ export class AssessmentBreakdownComponent implements OnInit {
     let currentTrainee = this.reportService.trainee;
 
     let gradeArray=[];
-    let assessmentArray=[];
+    let traineeAverageArray=[];
+    let traineeDisplayData=[];
+    let batchAverageArray=[];
+    let batchDisplayData=[];
     let students=[];
     let borderWidth=[];
+
     this.traineeDataStore.forEach((element)=>{
       gradeArray.push(0);
       students.push(element.name);
       borderWidth.push(2);
     });
+
+    // The assessmentMap correlates a assessments id to the actual assessment
+    let assessmentMap = new Map<number, Assessment> ()
+
+    this.assessmentDataStore.forEach((assessment) => {
+      assessmentMap.set(assessment.assessmentId, assessment);
+    });
+
+    batchAverageArray = this.getAverageGradeObject(assessmentMap, this.gradeDataStore);
+    console.log("Batch Averages");
+    console.log(batchAverageArray);
+    
 
     console.log(this.gradeDataStore);
     console.log(this.assessmentDataStore);
@@ -108,11 +134,12 @@ export class AssessmentBreakdownComponent implements OnInit {
       let grade = this.gradeDataStore[index];
 
       console.log("Assessment RAW SCORE: " + assessment.rawScore);
-      assessmentArray.push(assessment.rawScore);
-      gradeArray.push(grade.score);
+      // assessmentArray.push(assessment.rawScore);
+      // gradeArray.push(grade.score);
 
     }
-
+    
+  
     this.gradeDataStore.forEach((element)=>{
       
       let index = this.traineeDataStore.findIndex((trainee)=>{
@@ -133,14 +160,87 @@ export class AssessmentBreakdownComponent implements OnInit {
     console.log(gradeArray);
     ////////////// place grade.toFixed(2) loop here
     /////////// (maybe the issue was that it has to happen after sorting?)
-    this.barChartLabels = students;
+    // this.barChartLabels = students;
     this.gradeAverages = gradeArray;
+    this.barChartLabels = [];
+    this.tableHeader = [];
+    this.traineeRow = [];
+    this.batchRow = [];
+    let scoreString : string;
+    let assessmentTypeString : string;
+
+    batchAverageArray.forEach((assessmentScore: AssessmentScore) => {
+      //This rounds the assessment score to the first two decimals.
+      scoreString = ""+Math.round(assessmentScore.score*100)/100;
+
+      //This turns the assessment type to title case.
+      assessmentTypeString = assessmentScore.assessmentType.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+      });
+
+      batchDisplayData.push(scoreString);
+      this.barChartLabels.push(assessmentTypeString);
+      this.tableHeader.push(assessmentTypeString);
+      this.batchRow.push(scoreString);
+    });
+
+    let traineeData = {data: traineeDisplayData, label: 'Trainee', borderWidth: borderWidth};
+    let batchData = {data: batchDisplayData, label: 'Batch', borderWidth: borderWidth};
+
+    this.barChartData= [];
+    if (this.traineeSelected)
+      this.barChartData.push(traineeData);
     
-    this.barChartData= [
-      { data: gradeArray, label: 'Trainee', borderWidth: borderWidth},
-      { data: assessmentArray, label: 'Batch', borderWidth: borderWidth}
-    ];
+    this.barChartData.push(batchData);
   }
 
+  private getAverageGradeObject(assessmentMap: Map<number, Assessment>, allGrades: Grade[]) : AssessmentScore[] {
+    let currentAssessment: Assessment;
+    let sortedGrades = {};
+    let currentGradeType = "";
+    allGrades.forEach((grade) => {
+      currentAssessment = assessmentMap.get(grade.assessmentId)
+      currentGradeType = currentAssessment.assessmentType.toUpperCase();
+      if (!sortedGrades[currentGradeType]){
+        sortedGrades[currentGradeType] = [];
+      }
+      sortedGrades[currentGradeType].push(grade);
+    });
+    console.log("Sorted Grades");
+    console.log(sortedGrades);
+
+    let averageObject = {};
+    Object.keys(sortedGrades).forEach(type => {
+      let i = 0;
+      sortedGrades[type].forEach((grade: Grade) => {
+        if (!averageObject[type]){
+          averageObject[type] = 0;
+        }
+        averageObject[type] += grade.score;
+        i++;
+      });
+      averageObject[type] /= i;
+      console.log(type)
+      console.log(averageObject[type]);
+    });
+    
+    let assessmentAverage = [];
+    Object.keys(averageObject).forEach((type) => {
+      assessmentAverage.push(new AssessmentScore(type, averageObject[type]));
+    });
+    console.log(assessmentAverage);
+    
+    // This sorts the elements by alphabetically order and forces other to be at the end.
+    assessmentAverage.sort((assessment1 : AssessmentScore, assessment2 : AssessmentScore) => {
+      if (assessment1.assessmentType === 'OTHER') {
+        return 1;
+      }
+      else if (assessment2.assessmentType === 'OTHER') {
+        return -1;
+      }
+      return assessment1.assessmentType < assessment2.assessmentType ? -1 : assessment1.assessmentType > assessment2.assessmentType ? 1: 0;
+    })
+    return assessmentAverage;
+  }
 
 }
