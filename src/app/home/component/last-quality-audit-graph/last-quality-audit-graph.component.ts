693bfb20abe91f639b6443222f50cfc0ef195f0a
James Kellenberger <jwkellenberger@gmail.com>
2019-07-07 20:01:23 -0400

    organizing color of lastQualityAudit graph on home page

--- src/app/home/component/last-quality-audit-graph/last-quality-audit-graph.component.ts
+++ src/app/home/component/last-quality-audit-graph/last-quality-audit-graph.component.ts
@@ -47,16 +47,26 @@ export class LastQualityAuditGraphComponent implements OnInit {
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
-  public barChartColors: Array<any> = [
-    { 
-      backgroundColor: 'rgba(114, 164, 194, .5)',
-      borderColor: 'rgba(114, 164, 194, 1)',
-      pointBackgroundColor: 'rgba(252, 180, 20, .6)',
-      pointBorderColor: 'rgba(252, 180, 20, 1)',
-      pointHoverBackgroundColor: 'rgba(252, 180, 20, .6)',
-      pointHoverBorderColor: 'rgba(252, 180, 20, 1)',
-    }];
-    // public readonly defaultBorderColor2 = 'rgba(252, 180, 20, 1)';
+  // public barChartColors: Array<any> = [
+  //   { 
+  //     backgroundColor: 'rgba(114, 164, 194, .5)',
+  //     borderColor: 'rgba(114, 164, 194, 1)',
+  //     pointBackgroundColor: 'rgba(252, 180, 20, .6)',
+  //     pointBorderColor: 'rgba(252, 180, 20, 1)',
+  //     pointHoverBackgroundColor: 'rgba(252, 180, 20, .6)',
+  //     pointHoverBorderColor: 'rgba(252, 180, 20, 1)',
+  //   }];
+  //   // public readonly defaultBorderColor2 = 'rgba(252, 180, 20, 1)';
+
+  public readonly superstarBackgroundColor = 'rgba(57, 63, 239, 0.3)';
+  public readonly goodBackgroundColor = 'rgba(24, 173, 24, 0.3)';
+  public readonly averageBackgroundColor = 'rgba(249, 233, 0, 0.3)';
+  public readonly poorBackgroundColor = 'rgba(234, 40, 37, 0.3)';
+
+  public readonly superstarHoverColor = 'rgba(57, 63, 239, .35)';
+  public readonly goodHoverColor = 'rgba(24, 173, 24, .35)';
+  public readonly averageHoverColor = 'rgba(249, 233, 0, .35)';
+  public readonly poorHoverColor = 'rgba(234, 40, 37, .35)';
 
   public barChartData: ChartDataSets[] = [
     { data: [], label: 'Series A'}
@@ -93,6 +103,7 @@ export class LastQualityAuditGraphComponent implements OnInit {
     let goodArray: number[] = [];
     let starArray: number[] = [];
     let undefinedArray: number[] = [];
+    let borderWidth: number[] = [];
     
     let week: number;
     let index: number = 0;
@@ -110,7 +121,8 @@ export class LastQualityAuditGraphComponent implements OnInit {
         averageArray.push(0);
         goodArray.push(0);
         starArray.push(0);
-        undefinedArray.push(0);
+        // undefinedArray.push(0);
+        borderWidth.push(2);
 
         qaArray.forEach(
           (qaNote)=>{
@@ -120,7 +132,7 @@ export class LastQualityAuditGraphComponent implements OnInit {
                 case 'Average': averageArray[index] +=1; break;
                 case 'Good': goodArray[index] +=1; break;
                 case 'Superstar': starArray[index] +=1; break;
-                case 'Undefined': undefinedArray[index] +=1; break;
+                // case 'Undefined': undefinedArray[index] +=1; break;
               }
             }
           });
@@ -128,14 +140,12 @@ export class LastQualityAuditGraphComponent implements OnInit {
         console.log(qaArray[0].batchId)
         console.log(starArray)
       });
-
     this.barChartData= [
-      { data: poorArray, label: 'Poor', stack: 'a' },
-      { data: averageArray, label: 'Average', stack: 'a' },
-      { data: goodArray, label: 'Good', stack: 'a' },
-      { data: starArray, label: 'Superstar', stack: 'a' },
-      { data: undefinedArray, label: 'unidentidied', stack: 'a' },
+      { data: poorArray,      label: 'Poor',        borderWidth: borderWidth,  stack: 'a', backgroundColor: this.poorBackgroundColor,     borderColor: this.poorHoverColor,       hoverBackgroundColor: this.poorHoverColor,      hoverBorderColor: this.poorHoverColor,},
+      { data: averageArray,   label: 'Average',     borderWidth: borderWidth,  stack: 'a', backgroundColor: this.averageBackgroundColor,  borderColor: this.averageHoverColor,    hoverBackgroundColor: this.averageHoverColor,   hoverBorderColor: this.averageHoverColor,},
+      { data: goodArray,      label: 'Good',        borderWidth: borderWidth,  stack: 'a', backgroundColor: this.goodBackgroundColor,     borderColor: this.goodHoverColor,       hoverBackgroundColor: this.goodHoverColor,      hoverBorderColor: this.goodHoverColor,}, 
+      { data: starArray,      label: 'Superstar',   borderWidth: borderWidth,  stack: 'a', backgroundColor: this.superstarBackgroundColor,borderColor: this.superstarHoverColor,  hoverBackgroundColor: this.superstarHoverColor, hoverBorderColor: this.superstarHoverColor,},
+      // { data: undefinedArray, label: 'unidentidied',borderWidth: borderWidth,  stack: 'a', backgroundColor: 'rgba(114, 164, 194, .5)',    borderColor: 'rgba(114, 164, 194, .5)', hoverBackgroundColor: 'rgba(114, 164, 194, .5)',hoverBorderColor: 'rgba(114, 164, 194, .5)'},
     ];
   }
-
 }
