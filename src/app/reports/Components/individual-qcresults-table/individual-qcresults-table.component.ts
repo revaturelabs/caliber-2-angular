135fb0dddddb8a687a4122253066f902b25f094e
Denzell Gibson <denzellgibson@yahoo.com>
2019-07-08 19:47:34 -0400

    table finished

--- src/app/reports/Components/individual-qcresults-table/individual-qcresults-table.component.ts
+++ src/app/reports/Components/individual-qcresults-table/individual-qcresults-table.component.ts
@@ -11,24 +11,25 @@ import { Category } from 'src/app/Assess-Batch/Models/Category';
   styleUrls: ['./individual-qcresults-table.component.css']
 })
 export class IndividualQCResultsTableComponent implements OnInit {
-  qcData : QANote[];
-  categoryDataStore : Category[] = [];
-  assessmentDataStore : Assessment[]= [];
-  categoryForWeek : Category[] = [];
-  categoryId : number[] = [];
-  batchAssessmentDataStore : Assessment[] =[];
-  week : number;
-  batch : Batch;
-  message : String;
-  overallNote : QANote;
-  trainees;
-  public weekSelected : boolean = false;
+  qcData: QANote[];
+  categoryDataStore: Category[] = [];
+  assessmentDataStore: Assessment[] = [];
+  categoryForWeek: Category[] = [];
+  categoryId: number[] = [];
+  batchAssessmentDataStore: Assessment[] = [];
+  week: number;
+  batch: Batch;
+  message: String;
+  overallFeedback: number;
+  overallFeedbackString: String;
+  trainee;
+  public weekSelected: boolean;
 
 
 
-  constructor(private reportService : ReportService) {
-    if (!this.qcData){
-      this.qcData=[];
+  constructor(private reportService: ReportService) {
+    if (!this.qcData) {
+      this.qcData = [];
     }
    }
 
@@ -44,41 +45,118 @@ export class IndividualQCResultsTableComponent implements OnInit {
     this.qcData = this.reportService.getQANoteDataStore();
     this.categoryDataStore = this.reportService.getCategoryDataStore();
     this.assessmentDataStore = this.reportService.getAssessmentDataStore();
+    this.trainee = this.reportService.getTrainee();
 
     this.determineCategoryForWeek();
+    this.determineBatchAverage();
+    this.determineOverallString();
 
-    if (this.qcData == undefined || this.qcData.length === 0){
-      this.reportService.getAllQANotes().subscribe((qcNotes : QANote[])=>{
+    if (this.qcData === undefined || this.qcData.length === 0) {
+      this.reportService.getAllQANotes().subscribe((qcNotes: QANote[]) => {
         this.qcData = qcNotes;
-
         this.categoryDataStore = this.reportService.getCategoryDataStore();
         this.assessmentDataStore = this.reportService.getAssessmentDataStore();
+        this.trainee = this.reportService.getTrainee();
+
         this.determineCategoryForWeek();
+        this.determineBatchAverage();
+        this.determineOverallString();
       });
-      
-    }
-    else {
+    } else {
       this.qcData = this.reportService.getQANoteDataStore();
       this.categoryDataStore = this.reportService.getCategoryDataStore();
       this.assessmentDataStore = this.reportService.getAssessmentDataStore();
+      this.trainee = this.reportService.getTrainee();
+
       this.determineCategoryForWeek();
+      this.determineBatchAverage();
+      this.determineOverallString();
     }
     console.log(this.categoryForWeek);
+    console.log(this.overallFeedback);
+    console.log(this.determineOverallString);
+    console.log(this.trainee);
+    console.log(this.qcData);
   }
 
-  determineCategoryForWeek(){
-    for (let i = 0; i < this.assessmentDataStore.length; i++){
-      if (this.qcData[0].batchId == this.assessmentDataStore[i].batchId && this.qcData[0].week == this.assessmentDataStore[i].weekNumber){
-        if (this.categoryId.find(x => x == this.assessmentDataStore[i].assessmentCategory) === undefined){
-          this.categoryId.push(this.assessmentDataStore[i].assessmentCategory);
+  determineCategoryForWeek() {
+    this.categoryForWeek = [];
+    this.categoryId = [];
+    for (let i = 0; i < this.assessmentDataStore.length; i++) {
+      if (this.qcData[0].batchId === this.assessmentDataStore[i].batchId) {
+        if (this.qcData[0].week === this.assessmentDataStore[i].weekNumber) {
+          if (this.categoryId.find(x => x === this.assessmentDataStore[i].assessmentCategory) === undefined) {
+            this.categoryId.push(this.assessmentDataStore[i].assessmentCategory);
+          }
         }
       }
     }
-    for (let i = 0; i < this.categoryId.length; i++){
-      for (let j = 0; j < this.categoryDataStore.length; j++){
-        if (this.categoryId[i] == this.categoryDataStore[j].categoryId){
+    for (let i = 0; i < this.categoryId.length; i++) {
+      console.log(this.categoryId[i]);
+      for (let j = 0; j < this.categoryDataStore.length; j++) {
+        if (this.categoryId[i] === this.categoryDataStore[j].categoryId) {
+          if (this.categoryForWeek.includes(this.categoryDataStore[j]) === false) {
           this.categoryForWeek.push(this.categoryDataStore[j]);
+          console.log(this.categoryDataStore[j]);
+          }
         }
+      }
+    }
+  }
+
+  determineBatchAverage() {
+    let numberOfPeople = 0;
+    this.overallFeedback = 0;
+    for (let i = 0; i < this.qcData.length; i++) {
+      switch (this.qcData[i].qcStatus) {
+        case 'Superstar':
+          this.overallFeedback = this.overallFeedback + 4;
+          numberOfPeople += 1;
+          break;
+        case 'Good':
+            this.overallFeedback = this.overallFeedback + 3;
+          numberOfPeople += 1;
+          break;
+        case 'Average':
+            this.overallFeedback = this.overallFeedback + 2;
+          numberOfPeople += 1;
+          break;
+        case 'Poor':
+            this.overallFeedback = this.overallFeedback + 1;
+          numberOfPeople += 1;
+          break;
+        default:
+          break;
+      }
+    }
+    console.log('Raw Overall: ' + this.overallFeedback);
+    this.overallFeedback = this.overallFeedback / numberOfPeople;
+    console.log('Raw Average Overall: ' + this.overallFeedback);
+    this.overallFeedback = Math.round(this.overallFeedback);
+    console.log(this.overallFeedback);
+  }
+
+  determineOverallString() {
+    switch (this.overallFeedback) {
+      case 1: {
+        this.overallFeedbackString = 'Poor';
+        break;
+      }
+      case 2: {
+        this.overallFeedbackString = 'Average';
+        break;
+      }
+      case 3: {
+        this.overallFeedbackString = 'Good';
+        break;
+      }
+      case 4: {
+        this.overallFeedbackString = 'Superstar';
+        break;
+      }
+      default: {
+        this.overallFeedbackString = 'No data';
+        break;
       }
     }
   }
