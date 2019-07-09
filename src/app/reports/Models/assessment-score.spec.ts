dcf4ab0c306240d8dc018b85b75f3e1cc2c00c88
Patrick Call <patrickscall5@gmail.com>
2019-07-09 15:19:26 -0400

    fixed merge conflict inside assessment-score.spec.ts

--- src/app/reports/Models/assessment-score.spec.ts
+++ src/app/reports/Models/assessment-score.spec.ts
@@ -2,10 +2,6 @@ import { AssessmentScore } from './assessment-score';
 
 describe('AssessmentScore', () => {
   it('should create an instance', () => {
-<<<<<<< HEAD
-    expect(new AssessmentScore('', 0)).toBeTruthy();
-=======
     expect(new AssessmentScore("EXAM", 88)).toBeTruthy();
->>>>>>> 1d6fd36a18e121d516212f45292a8892be25c30d
   });
 });
