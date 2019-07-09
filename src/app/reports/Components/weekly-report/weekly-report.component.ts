7b73d5574eed487668429c43797f52b9ffdcc937
Vasquez Jonah <jonadiazz@hotmail.com>
2019-07-09 04:47:25 -0400

    chart by trainee
    Adding a line chart for each trainee along with batch line chart

--- src/app/reports/Components/weekly-report/weekly-report.component.ts
+++ src/app/reports/Components/weekly-report/weekly-report.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Assessment } from 'src/app/Assess-Batch/Models/Assesment';
-import { Grade } from 'src/app/Batch/type/trainee';
+import {Grade, Trainee} from 'src/app/Batch/type/trainee';
 import { ReportService } from '../../Service/report.service';
 
 @Component({
@@ -10,119 +10,199 @@ import { ReportService } from '../../Service/report.service';
 })
 export class WeeklyReportComponent implements OnInit {
 
-  private grades : Grade[];
+  private grades: Grade[];
+  private gradesByTrainee: Grade[];
   private assessments: Assessment[];
-  private weekAverages : Map<string,number[]> ;
+  private assessmentsByTrainee: Assessment[];
+  private weekAverages: Map<string, number[]> ;
+  private weekAveragesByTrainee: Map<string, number[]> ;
   public avgData;
-  public display : boolean;
+  public avgDataByTrainee;
+  public display: boolean;
+  public overallByTrainee: boolean;
 
   public chartOptions;
   public chartData;
   public chartLabels;
 
-
-  constructor(private reportService : ReportService) {
+  constructor(private reportService: ReportService) {
   }
 
   ngOnInit() {
     this.display = false;
-    this.weekAverages = new Map<string, number[]>();  
+    this.overallByTrainee = false;
+    this.weekAverages = new Map<string, number[]>();
+    this.weekAveragesByTrainee = new Map<string, number[]>();
     this.avgData = [];
+    this.avgDataByTrainee = [];
   }
 
   onChartClick(event) {
 
   }
 
-  
-  update(){
-    if(this.reportService.getWeek() ===0 && this.reportService.getTrainee().traineeId === -1)
-    {
+
+  update() {
+    this.display = false;
+    if (this.reportService.getWeek() === 0) {
       this.resetChart();
-      this.updateDataForAll();
+      const trainee = this.reportService.getTrainee();
+      if (trainee.traineeId === -1) {
+        this.overallByTrainee = false;
+        this.updateDataForAll();
+      } else {
+        this.overallByTrainee = true;
+        this.updateDataForTrainee(trainee);
+      }
     }
   }
 
-  updateDataForAll(){
+  updateDataForAll() {
     this.grades = this.reportService.getGradeDataStore();
     this.assessments = this.reportService.getAssessmentDataStore();
-    if(this.grades.length>0 && this.assessments.length>0)
-    {
+    if (this.grades.length > 0 && this.assessments.length > 0) {
       this.display = true;
-      let gradeAverages = [];
+      const gradeAverages = [];
       let tempAverage = 0;
       let tempCount = 0;
 
-      //matches each assessment to a week and puts it into an array
-      //matches each grade to an assessment and finds the average of all grades per assessment
-        this.assessments.forEach((tempAssessment)=>{
-          this.grades.forEach((tempGrade)=>{
-            if(tempAssessment.assessmentId == tempGrade.assessmentId){
-              tempCount++;
-              tempAverage+=tempGrade.score;
-            }
-          });
-          tempAverage /= tempCount;
-          gradeAverages.push(tempAverage);
-          tempAverage = 0;
-          tempCount = 0;
+      // matches each assessment to a week and puts it into an array
+      // matches each grade to an assessment and finds the average of all grades per assessment
+      this.assessments.forEach((tempAssessment) => {
+        this.grades.forEach((tempGrade) => {
+          if (tempAssessment.assessmentId == tempGrade.assessmentId) {
+            tempCount++;
+            tempAverage += tempGrade.score;
+          }
         });
+        tempAverage /= tempCount;
+        gradeAverages.push(tempAverage);
+        tempAverage = 0;
+        tempCount = 0;
+      });
 
-        for(let i=0; i<this.assessments.length; i++){
-          let tempWeekNumber = this.assessments[i].weekNumber;
-          let weekNumber = String(tempWeekNumber);
+      for (let i = 0; i < this.assessments.length; i++) {
+        const tempWeekNumber = this.assessments[i].weekNumber;
+        const weekNumber = String(tempWeekNumber);
 
-          if(this.weekAverages.has(weekNumber)){
-            let averages = this.weekAverages.get(weekNumber);
-            averages.push(gradeAverages[i]);
-            this.weekAverages.set(weekNumber, averages);
-          }
-          else{
-            this.weekAverages.set(weekNumber, [gradeAverages[i]]);
-          }
+        if (this.weekAverages.has(weekNumber)) {
+          const averages = this.weekAverages.get(weekNumber);
+          averages.push(gradeAverages[i]);
+          this.weekAverages.set(weekNumber, averages);
+        } else {
+          this.weekAverages.set(weekNumber, [gradeAverages[i]]);
         }
-        //generate the chart
-        this.fillChart();   
       }
-      else{
-        this.display = false;
-      }
+      // generate the chart
+      this.fillChart();
+    } else {
+      this.display = false;
+    }
   }
 
-  fillChart(){
-    
+  fillChart() {
+
     this.chartOptions = {
     };
 
-    let keys = Array.from(this.weekAverages.keys());
-    //fill line
-    keys.forEach((key)=>{
+    const keys = Array.from(this.weekAverages.keys());
+    // fill line
+    keys.forEach((key) => {
       let avg = 0;
-      this.weekAverages.get(key).forEach((grade)=>{
+      this.weekAverages.get(key).forEach((grade) => {
         avg += grade;
       });
-      //add the rounded averages into an array
-      this.avgData.push( Math.round( avg/this.weekAverages.get(key).length *100) /100);
+      // add the rounded averages into an array
+      this.avgData.push( Math.round( avg / this.weekAverages.get(key).length * 100) / 100);
     });
 
+    const keysByTrainee = Array.from(this.weekAveragesByTrainee.keys());
+    keysByTrainee.forEach((key) => {
+      let avg = 0;
+      this.weekAveragesByTrainee.get(key).forEach((grade) => {
+        avg += grade;
+      });
+      this.avgDataByTrainee.push( Math.round(avg / this.weekAveragesByTrainee.get(key).length * 100) / 100);
+    });
+
+    // keys.map( (obj) => {
+    //   obj = "Trainee" + obj;
+    //   return obj;
+    // });
     this.chartData = [
-      { 
-        data: this.avgData, 
-        fill: false, 
+      {
+        data: this.avgData,
+        fill: false,
         borderColor: 'rgba(114,164,194,1)',
       },
+      {
+        data: this.avgDataByTrainee,
+        fill: false,
+        borderColor: 'rgba(255,191,37,1)',
+      },
     ];
-    
-    //set x axis labels
+
+    // set x axis labels
     this.chartLabels = keys;
+    // keys.b( (obj) => {
+    //   obj = "Trainee" + obj;
+    //   return obj;
+    // });
   }
 
 
-  resetChart(){
-    this.chartData=[];
-    this.chartLabels=[];
+  resetChart() {
+    this.chartData = [];
+    this.chartLabels = [];
     this.avgData = [];
+    this.avgDataByTrainee = [];
     this.chartOptions = [];
   }
 
+  private updateDataForTrainee(trainee: Trainee) {
+    this.gradesByTrainee = this.reportService.getGradeDataStore();
+    this.assessmentsByTrainee = this.reportService.getAssessmentDataStore();
+    // If there are no grades or assessments then set flag to display chart to false
+    if (!(this.gradesByTrainee.length > 0 && this.assessmentsByTrainee.length > 0)) {
+      this.display = false;
+    } else {
+      this.display = true;
+      const gradeAverages = [];
+      let tempAverage = 0;
+      let tempCount = 0;
+
+      // matches each assessment to a week and puts it into an array
+      // matches each grade to an assessment and finds the average of all grades per assessment
+      this.assessmentsByTrainee.forEach((tempAssessment) => {
+        this.gradesByTrainee.forEach((tempGrade) => {
+          if (tempAssessment.assessmentId == tempGrade.assessmentId) {
+            if (trainee.traineeId == tempGrade.traineeId) {
+              tempCount++;
+              tempAverage += tempGrade.score;
+            }
+
+          }
+        });
+        tempAverage /= tempCount;
+        gradeAverages.push(tempAverage);
+        tempAverage = 0;
+        tempCount = 0;
+      });
+
+      for (let i = 0; i < this.assessmentsByTrainee.length; i++) {
+        const tempWeekNumber = this.assessmentsByTrainee[i].weekNumber;
+        const weekNumber = String(tempWeekNumber);
+
+        if (this.weekAveragesByTrainee.has(weekNumber)) {
+          const averages = this.weekAveragesByTrainee.get(weekNumber);
+          averages.push(gradeAverages[i]);
+          this.weekAveragesByTrainee.set(weekNumber, averages);
+        } else {
+          this.weekAveragesByTrainee.set(weekNumber, [gradeAverages[i]]);
+        }
+      }
+      // generate the chart
+      this.fillChart();
+    }
+  }
 }
