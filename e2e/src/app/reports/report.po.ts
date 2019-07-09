903a081b5312075e417690b0e8a6a93919431ff3
Anand <apanchal2@gmail.com>
2019-07-09 15:29:08 -0400

    deleted few comments

--- e2e/src/app/reports/report.po.ts
+++ e2e/src/app/reports/report.po.ts
@@ -1,16 +1,6 @@
 import { browser, by, element, ElementFinder } from "protractor";
 
 
-// const viewReportsURL = 'http://localhost:4200/caliber/vp/reports';
-// export class ReportPage {
-//     navigateTo() {
-//         return browser.get(viewReportsURL);
-//         // element(by.css('app-root #reports-link'));
-//     }
-
-
-// }
-
 export class ViewToolBarInReportsPage {
     navigateTo() {
         return browser.get('/caliber/vp/reports');
