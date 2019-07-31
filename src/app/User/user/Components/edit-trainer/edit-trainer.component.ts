15a45d57453bbafe2bd7b30f0cc4503e1951b516
Carl Pacquing <cjpacquing@gmail.com>
2019-07-31 15:26:04 -0400

    Update and Close Modal Reconfigured.

--- src/app/User/user/Components/edit-trainer/edit-trainer.component.ts
+++ src/app/User/user/Components/edit-trainer/edit-trainer.component.ts
@@ -4,6 +4,7 @@ import { TrainersService } from '../../Services/trainers.service';
 import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorService } from 'src/app/error-handling/services/error.service';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-trainer',
@@ -18,54 +19,42 @@ export class EditTrainerComponent implements OnInit, OnChanges {
   @Output() refreshList = new EventEmitter<boolean>();
   //Used to store the original trainer fields.
   originalTrainer = new Trainer();
-  trainerTemp: Trainer;
-  constructor(private trService: TrainersService, private errorService: ErrorService, private router:Router) { }
+  //Used to store the new trainer inputs.
+  trainer:Trainer;
+
+  constructor(private trService: TrainersService, private errorService: ErrorService, private router:Router ) { }
 
   ngOnChanges(){
-    // if(this.trainerToEdit){
-    //   this.originalTrainer = this.trainerToEdit;
-    // }
+    console.log("Something changed.");
   }
 
   ngOnInit() 
   {
-    //console.log();
+    
   }
 
-  displayTrainer(trainer:Trainer)
+  displayTrainer(trainerObj:Trainer)
   { //A setter method to set our initial Trainer information.
     //Display the information in the modal.
-    console.log(trainer);
+    console.log(trainerObj);
     //console.log(this.originalTrainer);
-    this.originalTrainer = trainer; 
-    this.setOriginalTrainer(trainer);
-    //this.trainerTemp = trainer;
+    this.originalTrainer = trainerObj;//Store original trainer to the trainerObj parameter.    
+    this.trainer = JSON.parse(JSON.stringify(trainerObj)); // actual trainer data;
     console.log(this.originalTrainer);
-    console.log(this.trainerTemp);
   }
 
-  setOriginalTrainer(trainer:Trainer){
-    // console.log(this.trainerTemp);
-    // console.log(this.originalTrainer);
-    console.log(trainer);
-    this.trainerTemp = trainer;
-  }
 
   updateTrainerToEdit(){
         //Update the trainer
-        this.trainerToEdit = this.originalTrainer;
+       // this.trainerToEdit = this.originalTrainer;
         console.log("Updating trainer");
-        this.trService.editTrainer(this.trainerToEdit).subscribe(trainer => {
+        console.log("Original trainer: ", this.originalTrainer);
+        console.log("New Trainer: ", this.trainer);
+        this.trService.editTrainer(this.trainer).subscribe(response => {
           console.log("Hello");
-          console.log(trainer);//prints null.
-          //this.router.navigate(['vp/trainers']);
-          // if (trainer) {
-          //   console.log("if reached.")
-          //   const elem = document.getElementById('closeButton1');
-          //   const evt = new MouseEvent('click', { bubbles: true });
-          //   elem.dispatchEvent(evt);
-          //   this.refreshList.emit(true);
-          // }
+          //this.originalTrainer= this.trainer;
+          //console.log("After Update: ", this.originalTrainer);
+          this.router.navigateByUrl("vp/trainers");
         },
         issue => {
           if (issue instanceof HttpErrorResponse) {
@@ -77,17 +66,17 @@ export class EditTrainerComponent implements OnInit, OnChanges {
       });
   }
 
-  closeTrainer(){
+  closeTrainer(form:NgForm){
+    form.reset();
     //This function should close the modal, and omit any changes made in the modal.
     //I.e. The trainer information should remain unchanged.
     console.log("Revert to original fields.");
-    //this.setOriginalTrainer();
-    // console.log(this.originalTrainer);
-    console.log(this.trainerTemp);
-    // console.log(this.trainerToEdit);
-    this.trainerToEdit = this.originalTrainer;
+    console.log("Original Trainer:",this.originalTrainer);
+    console.log("TrainerToEdit:", this.trainer);
+    this.trainer = this.originalTrainer;
+    console.log("After Revert:", this.trainer);
     //this.router.navigate(['vp/trainers']);
-    this.refreshList.emit(true);
+    //this.refreshList.emit(true);
   }
 
   getTrainerToEdit(trainer:Trainer)
