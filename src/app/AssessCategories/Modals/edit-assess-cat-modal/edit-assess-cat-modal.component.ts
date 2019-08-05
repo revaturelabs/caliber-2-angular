8107bbc2098a4867166e23ba3cdd96e1ad7cc0dc
Ryan Hodder <i_ryan.h@hotmail.com>
2019-08-05 16:17:44 -0400

    More category display fixes

--- src/app/AssessCategories/Modals/edit-assess-cat-modal/edit-assess-cat-modal.component.ts
+++ src/app/AssessCategories/Modals/edit-assess-cat-modal/edit-assess-cat-modal.component.ts
@@ -10,7 +10,8 @@ import { Category } from 'src/app/User/user/types/trainee';
 export class EditAssessCatModalComponent implements OnInit {
 
   category:Category = new Category();  //The category that will be selected for presentation and editing 
-  categories:Category [];     //The list of categories to be loaded and displayed 
+  dumbCat: Category = new Category();
+  categories:any []; //The list of categories to be loaded and displayed 
   errorMessage:string;
   successMessage:string;
   displayResultError:boolean;
@@ -27,6 +28,7 @@ export class EditAssessCatModalComponent implements OnInit {
   //sets the selected category as the local category to be edited 
   selected(cat:Category){
     this.category = cat;
+    this.dumbCat = this.category;
   }
 
   //gets the categories from the database and inserts them into the category list 
@@ -42,16 +44,28 @@ export class EditAssessCatModalComponent implements OnInit {
   //saves the changes made to the category into the database 
   save(){
     this.categoryService.edit(this.category.categoryId,this.category.categoryOwner, this.category.skillCategory, this.category.active).subscribe((res)=>{
-      var c = JSON.parse(JSON.stringify(res));
+      if(res != null){
+        let myJSON = JSON.stringify(res);
+        let result = JSON.parse(myJSON);
 
-      this.categories = c;
+        // this.categories = c;
+        this.displayResultSuccess = true;
+        this.successMessage = result.skillCategory + " has been successfully updated";
+      }
+    },(err)=>{
+      this.displayResultError = true;
+      this.errorMessage = err.error.message;
     });
+    this.displayResultSuccess = false;
+    this.displayResultError = false;
+    // this.category.skillCategory = '';
+    // this.category.categoryOwner = '';
   }
 
   clearModal(){
     this.displayResultSuccess = false;
     this.displayResultError = false;
-    this.category.skillCategory = '';
-    this.category.categoryOwner = '';
+    // this.category.skillCategory = '';
+    // this.category.categoryOwner = '';
   }
 }
