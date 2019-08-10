b2eb8d1771b5ca69e838f58909f5d39c99fc6070
toney.darion <toney.darion@gmail.com>
2019-08-10 12:29:57 -0500

    form-modal.component and overall.component tests

--- src/app/Audit/Components/overall/overall.component.spec.ts
+++ src/app/Audit/Components/overall/overall.component.spec.ts
@@ -21,23 +21,23 @@ describe('OverallComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ 
+      declarations: [
         OverallComponent,
-       ],
+      ],
       imports: [
         FormsModule,
         HttpClientTestingModule,
       ],
       providers: [AuditService],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(OverallComponent);
     component = fixture.componentInstance;
-    component.note = new QcNote(1,'test note',1,1,null,0,'QC_BATCH','Undefined',0,null,0);
-    overallNote = new QcNote(102,"batchNote102",1,2,null,0,"QC_BATCH","Undefined",1559573404871,null,null);
+    component.note = new QcNote(1, 'test note', 1, 1, null, 0, 'QC_BATCH', 'Undefined', 0, null, 0);
+    overallNote = new QcNote(102, "batchNote102", 1, 2, null, 0, "QC_BATCH", "Undefined", 1559573404871, null, null);
     auditService = fixture.debugElement.injector.get(AuditService);
 
     subscribeBatchNoteSpy = spyOn(auditService.overallBatchNoteChanged, 'subscribe').and.callThrough();
@@ -59,6 +59,7 @@ describe('OverallComponent', () => {
   });
 
   it('clears the saving icon', () => {
+    component.clearAllSavingIcon();
     expect(component.isSpinning).toBe(false);
     expect(component.isCheck).toBe(false);
     expect(component.isError).toBe(false);
@@ -73,16 +74,25 @@ describe('OverallComponent', () => {
   it('saves the note', () => {
     sendNoteSpy.and.returnValue(of(1));
     component.isTyping = true;
-    component.noteOnBlur(1,false);
+    component.noteOnBlur(1, false);
     expect(sendNoteSpy).toHaveBeenCalledWith(component.note);
     expect(component.isCheck).toBe(true);
   });
 
   it('shows a failed save', () => {
-      sendNoteSpy.and.returnValue(throwError(new HttpErrorResponse({status:400})));
-      component.noteOnBlur(1,false);
-      expect(sendNoteSpy).toHaveBeenCalledWith(component.note);
-      expect(component.isError).toBe(true);
-      expect(component.isCheck).toBe(false);
-    });
+    sendNoteSpy.and.returnValue(throwError(new HttpErrorResponse({ status: 400 })));
+    component.noteOnBlur(1, false);
+    expect(sendNoteSpy).toHaveBeenCalledWith(component.note);
+    expect(component.isError).toBe(true);
+    expect(component.isCheck).toBe(false);
+  });
+
+  it('should set score for QC note', () =>{
+    sendNoteSpy.and.returnValue(of(1));
+    component.setScore("yellow",1);
+    expect(sendNoteSpy).toHaveBeenCalledWith(component.note);
+    expect(component.note.qcStatus).toBe("yellow");
+    expect(subscribeBatchNoteSpy).toHaveBeenCalled();
+  });
+
 });
