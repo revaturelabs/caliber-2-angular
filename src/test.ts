85834543d34742e9ce43a1893ac7f7b82f2a9629
toney.darion <toney.darion@gmail.com>
2019-08-11 12:09:54 -0500

    2 test for feedback.component

--- src/test.ts
+++ src/test.ts
@@ -15,6 +15,7 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /associate\.component\.spec\.ts$/);
+//const context = require.context('./', true, /associate\.component\.spec\.ts$/);
+const context = require.context('./', true, /feedback\.component\.spec\.ts$/);
 // And load the modules.
 context.keys().map(context);
