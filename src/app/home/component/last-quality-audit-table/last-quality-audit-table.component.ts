3b629813020eeb6ba4924911cd03001410717b1d
sinhky01 <kyle.sinhart@gmail.com>
2019-08-22 15:01:33 -0400

    fixed the last audit table

--- src/app/home/component/last-quality-audit-table/last-quality-audit-table.component.ts
+++ src/app/home/component/last-quality-audit-table/last-quality-audit-table.component.ts
@@ -21,7 +21,7 @@ export class LastQualityAuditTableComponent implements OnInit {
   private qaNoteDataStore: QANote[][];
   constructor(private homeService: HomeService) {
     this.statuses = ['Poor', 'Average', 'Good', 'Superstar', 'Overall Batch Status'];
-   }
+  }
 
   ngOnInit() {
   }
@@ -38,8 +38,18 @@ export class LastQualityAuditTableComponent implements OnInit {
     this.overallStatusArray = [];
     this.statusColorArray = [];
 
+    const batchIdLead: number[] = [];
+    this.batchDataStore.forEach(
+      (batch) => {
+        batchIdLead.push(batch.batchId);
+      }
+    )
+    const batchIdFollow: number[] = [];
+
+
     let week: number;
     let index = 0;
+
     this.qaNoteDataStore.forEach(
       (qaArray) => {
         week = 0;
@@ -49,6 +59,7 @@ export class LastQualityAuditTableComponent implements OnInit {
               week = qaNote.week;
             }
           });
+        batchIdFollow.push(qaArray[0].batchId);
         this.poorArray.push(0);
         this.averageArray.push(0);
         this.goodArray.push(0);
@@ -72,18 +83,77 @@ export class LastQualityAuditTableComponent implements OnInit {
               } else {
                 this.overallStatusArray[index] = qaNote.qcStatus;
               }
-              switch (qaNote.qcStatus) {
-                case 'Undefined': color = 'orange'; break;
-                case 'Poor': color = 'red'; break;
-                case 'Average': color = 'yellow'; break;
-                case 'Good': color = 'green'; break;
-                case 'Superstar': color = 'blue'; break;
-              }
-              this.statusColorArray[index] = color;
+              // switch (qaNote.qcStatus) {
+              //   case 'Undefined': color = 'orange'; break;
+              //   case 'Poor': color = 'red'; break;
+              //   case 'Average': color = 'yellow'; break;
+              //   case 'Good': color = 'green'; break;
+              //   case 'Superstar': color = 'blue'; break;
+              // }
+              // this.statusColorArray[index] = color;
             }
-        });
+          });
         index++;
-  });
-}
+      });
+
+    let batchIdLeader: number[] = [];
+
+    for (let i = 0; i < batchIdLead.length; i++) {
+      if (batchIdFollow.includes(batchIdLead[i])) {
+        batchIdLeader.push(batchIdLead[i]);
+      }
+    }
+
+    batchIdLeader.forEach((element, ctIndex) => {
+      if (ctIndex < batchIdFollow.length && element !== batchIdFollow[ctIndex]) {
+        let i: number = 0;
+        let temp: number;
+        let tempWeek: number;
+        let poorArrayTemp: number;
+        let averageArrayTemp: number;
+        let goodArrayTemp: number;
+        let starArrayTemp: number;
+        let overallStatusArrayTemp: String;
+        for (i = 0; i < batchIdFollow.length; i++) {
+          if (batchIdFollow[i] == element) {
+            temp = batchIdFollow[i];
+            //tempWeek = this.chartWeekReference[i];
+            poorArrayTemp = this.poorArray[i];
+            averageArrayTemp = this.averageArray[i];
+            goodArrayTemp = this.goodArray[i];
+            starArrayTemp = this.starArray[i];
+            overallStatusArrayTemp=this.overallStatusArray[i];
+
+            batchIdFollow[i] = batchIdFollow[ctIndex];
+            //this.chartWeekReference[i] = this.chartWeekReference[ctIndex];
+            this.poorArray[i] = this.poorArray[ctIndex];
+            this.averageArray[i] = this.averageArray[ctIndex];
+            this.goodArray[i] = this.goodArray[ctIndex];
+            this.starArray[i] = this.starArray[ctIndex];
+            this.overallStatusArray[i] = this.overallStatusArray[ctIndex];
+
+
+            batchIdFollow[ctIndex] = temp;
+            //this.chartWeekReference[ctIndex] = tempWeek;
+            this.poorArray[ctIndex] = poorArrayTemp;
+            this.averageArray[ctIndex] = averageArrayTemp;
+            this.goodArray[ctIndex] = goodArrayTemp;
+            this.starArray[ctIndex] = starArrayTemp;
+            this.overallStatusArray[ctIndex] = overallStatusArrayTemp;
+
+            let color;
+            switch (overallStatusArrayTemp) {
+              case 'Undefined': color = 'orange'; break;
+              case 'Poor': color = 'red'; break;
+              case 'Average': color = 'yellow'; break;
+              case 'Good': color = 'green'; break;
+              case 'Superstar': color = 'blue'; break;
+            }
+            this.statusColorArray[ctIndex] = color;
+          }
+        }
+      }
+    });
+  }
 
 }
