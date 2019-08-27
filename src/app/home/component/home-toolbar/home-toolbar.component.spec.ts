abb3f294242901518859ed3c0f76e562f525e84a
sinhky01 <kyle.sinhart@gmail.com>
2019-08-27 13:14:30 -0400

    in progress changes

--- src/app/home/component/home-toolbar/home-toolbar.component.spec.ts
+++ src/app/home/component/home-toolbar/home-toolbar.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HomeToolbarComponent } from './home-toolbar.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { componentNeedsResolution } from '@angular/core/src/metadata/resource_loading';
 
 describe('HomeToolbarComponent', () => {
   let component: HomeToolbarComponent;
@@ -25,5 +26,36 @@ describe('HomeToolbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('test showState()',()=>{
+    expect(component.showStates).toBe(false);
+    component.calShowState(component.states[0]);
+    expect(component.showStates).toBe(true);
+  });
+
+  it('test selectState',()=>{
+    
+  });
+  it('test selectStateAndCity',()=>{
+
+  });
+  it('test selectCity',()=>{
+
+  });
+  it('test initializeAllLocations',()=>{
+
+  });
+  it('test initializeCurrentBatches',()=>{
+
+  });
+  it('test initializeCurrentBatchesFromLocations',()=>{
+
+  });
+  it('test initilaizeAllQANotes',()=>{
+
+  });
+  it('test setStatesViaLocations',()=>{
+
   });
 });
