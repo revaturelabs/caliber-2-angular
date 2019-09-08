7ed0445e9f820c6b09b25433aff354a1718094db
Quintin Donnelly <quintin.donnelly@revature.com>
2019-09-08 14:44:46 -0400

    Update to point to the api gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@
  */
 export const environment = {
   production: false,
-  serverRootURL: 'http://caliber-2-dev-alb-315997072.us-east-1.elb.amazonaws.com'
+  serverRootURL: 'https://tdfnhh9ryi.execute-api.us-east-1.amazonaws.com/dev'
 };
